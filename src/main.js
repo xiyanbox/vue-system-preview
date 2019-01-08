a1ced80cf2b0137e781565a48a51bdba183ea663
weilong.Ma <13735800742@163.com>
2019-01-09 00:30:00 +0800

    qs&&axios

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import ElementUI from 'element-ui';
-import Vuex from 'vuex'
+import Vuex from 'vuex';
 import 'element-ui/lib/theme-chalk/index.css';
-import 'normalize.css/normalize.css' 
+import 'normalize.css/normalize.css' ;
+import axios from 'axios';
+import QS from 'qs';
+Vue.prototype.$axios = axios;
+Vue.prototype.qs = QS;
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 Vue.use(ElementUI);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
