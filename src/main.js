56f8d9f7032dee0f3e096f975af19376b3da4797
weilong.Ma <13735800742@163.com>
2019-01-09 00:16:55 +0800

    create store

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
+import Vuex from 'vuex'
 import 'element-ui/lib/theme-chalk/index.css';
 import 'normalize.css/normalize.css' 
 
+Vue.use(Vuex)
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false
