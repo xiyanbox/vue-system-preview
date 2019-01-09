542d634c4fc1dcc129946e475aa44b53498f6331
LAPTOP-S5MG9Q3G\cuide <565946416@qq.com>
2019-01-09 09:07:15 +0800

    cdy 修改错了

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Button, Select } from 'element-ui'
 
 Vue.config.productionTip = false
+Vue.use(Button)
+Vue.use(Select)
 
 new Vue({
   render: h => h(App),
